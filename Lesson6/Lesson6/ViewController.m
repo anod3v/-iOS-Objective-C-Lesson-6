2fdc23402f89b4c1dbe3b7e42e935570cfd1eb50
Anoshkin <a.anoshkin@profsoft.pro>
2021-02-13 21:01:03 +0300

    started doing blocks

--- Lesson6/Lesson6/ViewController.m
+++ Lesson6/Lesson6/ViewController.m
@@ -9,6 +9,8 @@
 
 @interface ViewController ()
 
+typedef int(^SquareBlock)(int);
+
 @end
 
 @implementation ViewController
@@ -16,7 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    
+    SquareBlock square = ^(int number) {
+        return number * number;
+    };
+
+    dispatch_async(mainQueue, ^{
+        int result = square(3);
+        NSLog(@"Result - %i", result); // Result - 9
+    });
 }
+
+        
+        
+        
 
 
 @end
