152b2db3d17800991329206677e7c906aac20a07
Anoshkin <a.anoshkin@profsoft.pro>
2021-02-17 22:07:48 +0300

    doing queues

--- Lesson6/Lesson6/ViewController.m
+++ Lesson6/Lesson6/ViewController.m
@@ -5,11 +5,14 @@
 //  Created by Andrey on 13/02/2021.
 //
 
+// программа калькулятор с применением блоков и очередей
+
 #import "ViewController.h"
 
 @interface ViewController ()
 
 typedef int(^SquareBlock)(int);
+typedef int(^SumBlock)(int);
 
 @end
 
@@ -18,21 +21,69 @@ typedef int(^SquareBlock)(int);
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    // ---
     
-    SquareBlock square = ^(int number) {
-        return number * number;
+    dispatch_queue_t globalQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    
+    double (^multiplyTwoValues)(double, double) =
+    ^(double firstValue, double secondValue) {
+        return firstValue * secondValue;
     };
-
-    dispatch_async(mainQueue, ^{
-        int result = square(3);
-        NSLog(@"Result - %i", result); // Result - 9
+    
+    dispatch_async(globalQueue, ^{
+        double result1 = multiplyTwoValues(2,4);
+        NSLog(@"Result1 - %f", result1);
     });
+    
+    double (^devideTwoValues)(double, double) =
+    ^(double firstValue, double secondValue) {
+        return firstValue / secondValue;
+    };
+    dispatch_async(globalQueue, ^{
+        double result2 = devideTwoValues(2,4);
+        NSLog(@"Result2 - %f", result2);
+    });
+    
+    double (^subtractTwoValues)(double, double) =
+    ^(double firstValue, double secondValue) {
+        return firstValue - secondValue;
+    };
+    dispatch_async(globalQueue, ^{
+        double result3 = subtractTwoValues(2,4);
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(@"Result3 - %f", result3);
+        });
+    });
+    
+    double (^sumTwoValues)(double, double) =
+    ^(double firstValue, double secondValue) {
+        return firstValue + secondValue;
+    };
+    
+    double result4 = sumTwoValues(2,4);
+    
+    NSLog(@"Result4 - %f", result4);
+    
+    double (^raiseToPower)(double, double) =
+    ^(double firstValue, double power) {
+        return (double)pow((double)firstValue, (double)power);
+    };
+    
+    double result5 = raiseToPower(2,4);
+    
+    NSLog(@"Result5 - %f", result5);
+    
+    double (^rootOfPower)(double, double) =
+    ^(double firstValue, double power) {
+        return (double)pow((double)firstValue, 1/(double)power);
+    };
+    
+    double result6 = rootOfPower(2,4);
+    
+    NSLog(@"Result6 - %f", result6);
 }
 
-        
-        
-        
+
 
 
 @end
